08c2d1454e8530b10b79b3c38b5bac9349f0129f
Trevor F. Smith <trevor@trevor.smith.name>
2009-12-18 11:19:27 -0800

    Set the art_settings to something which is not specific to our setup.

--- scripts/art_settings.py
+++ scripts/art_settings.py
@@ -3,10 +3,10 @@
 INSTALLATION_ID = None
 
 # The IP (and optionally port) of the public server running in the cloud
-CLOUD_HOST = "174.129.3.149"
+CLOUD_HOST = "127.0.0.1:8080"
 
 # The IP (and optionally port) of the on-site server
-ART_SERVER_HOST = "172.16.220.40"
+ART_SERVER_HOST = "127.0.0.1:8000"
 
 # The timeout after which the weather client will fail if it hasn't had a response
 WEATHER_TIMEOUT = 80 # in seconds
@@ -14,8 +14,3 @@ WEATHER_TIMEOUT = 80 # in seconds
 # The port on which the status listener will sit
 # Change this if you receive a error like 'Address already in use'
 STATUS_WEB_PORT = 8090
-
-# Some debug settings
-#INSTALLATION_ID = 21
-#CLOUD_HOST = "127.0.0.1:8080"
-#ART_SERVER_HOST = "127.0.0.1:8000"
